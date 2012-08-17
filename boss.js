54e058fe341ec79875899788043ff630b8cd2f1e
Yehor Lvivski <lvivski@gmail.com>
2012-08-18 00:01:48 +0300

    initial lexer

---
+++ boss.js
@@ -0,0 +1,19 @@
+var Lexer = require('./lib/lexer');
+
+var css = '\
+.boxed-group \n\
+  position: relative;\n\
+  background: -webkit-linear-gradient(left, #edd 90%, #edd 60%);\n\
+  padding: 3px;\n\
+  border-radius: 3px;\n\
+  margin:0 0 15px 0;\n\
+  .inner\n\
+    color:red\n\
+    background:url(/sd*i&fsd/sdf.png);\n\
+\n'
+
+try {
+    console.log(new Lexer(css).tokenize());
+} catch (e) {
+    console.log(e.stack)
+}
