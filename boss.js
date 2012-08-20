4fd89518bff77f167ec10472b0210404803e6e65
Yehor Lvivski <lvivski@gmail.com>
2012-08-21 00:24:31 +0300

    added rewriter

--- boss.js
+++ boss.js
@@ -1,21 +1,36 @@
-var Lexer = require('./lib/lexer'),
-    nodes = require('./lib/nodes');
+var Lexer = require('./lib/lexer')
+  , Rewriter = require('./lib/rewriter')
+  , nodes = require('./lib/nodes')
 
 var css = '\
-.boxed-group \n\
-  position: relative;\n\
-  background: -webkit-linear-gradient(left, #edd 90%, #edd 60%);\n\
-  padding: 3px;\n\
-  border-radius: 3px;\n\
-  margin:0 0 15px 0;\n\
-  .inner\n\
-    color:red\n\
-    background:url(/sd*i&fsd/sdf.png);\n\
+@-webkit-keyframes \'sdf\'\n\
+  0% {\n\
+    -webkit-linear-gradient: sef;\n\
+  }\n\
+\n\
+.boxed-group .tyty > tyty,\n\
+.sdf, \n\
+sdfsdf\n\
+  position : relative;\n\
+  background : -webkit-linear-gradient(left, #edd 90%, #edd 60%);\n\
+  padding : color 3px;\n\
+  border-radius : 3px\n\
+  margin :0 0 15px 0;\n\
+  &:ntg(sdf),\n\
+  & sfg sfd \n\
+  \n\
+  \n\
+  \n\
+  \n\
+  {\n\
+    color :red\n\
+    background : url(/sd*i&fsd/sdf.png),\n\
+                #fff;\n\
+              }\n\
 \n'
 
 try {
     console.log(new Lexer(css).tokenize())
-    console.log((new nodes.Dimension(500, 'ms')).operate('+', new nodes.Dimension(3, 's')))
 } catch (e) {
     console.log(e.stack)
 }
