9254b05d21c18c191f76523e98563cba50a7dafa
Yehor Lvivski <lvivski@gmail.com>
2012-08-23 16:01:57 +0300

    fixed some parser errors

--- boss.js
+++ boss.js
@@ -1,39 +1,33 @@
-var Parser = require('./lib/parser')
+var Compiler = require('./lib/compiler')
+  , Parser = require('./lib/parser')
+  , Lexer = require('./lib/lexer')
   , nodes = require('./lib/nodes')
   , inspect = require('util').inspect
 
 var css = '\
-@-webkit-keyframes \'sdf\'\n\
-  0% {\n\
-    -webkit-linear-gradient: sef;\n\
-  }\n\
-\n\
-.boxed-group .tyty > tyty,\n\
-.sdf, \n\
-sdfsdf\n\
-  position : relative;\n\
-  background : -webkit-linear-gradient(left, #edd 90%, #edd 60%);\n\
-  padding : color 3px;\n\
-  border-radius : 3px\n\
-  margin :0 0 15px 0;\n\
-  &:ntg(sdf),\n\
-  & sfg sfd \n\
-  \n\
-  \n\
-  \n\
-  \n\
-  {\n\
-    color :red\n\
-    background : url(/sd*i&fsd/sdf.png),\n\
-                #fff;\n\
-              }\n\
+block\n\
+  background -webkit-linear-gradient(left, #edd 90%, #edd 60%)\n\
+  &:mod(value)\n\
+    padding 0px 2px + 10px 0 0\n\
+  &elem\n\
+    padding 0px 2em + 10% 0px -2px\n\
+    background url(some/image/url.png), #fff\n\
+    &:elem-mod(value)\n\
+      color #fff\n\
+  &:hover\n\
+    color red\n\
 \n'
 
 try {
+    // var lexer = new Lexer(css)
+    // console.log(lexer.tokenize())
     var parser = new Parser(css);
-    var ast = parser.parse();
+    //console.log(inspect(parser.parse(), false, null))
+    // console.log(parser.parse().toString())
+    var compiler = new Compiler(parser.parse())
+    // compiler.compile()
+    console.log(compiler.compile())
 
-    console.log(inspect(ast, false, null))
 } catch (e) {
     console.log(e.stack)
 }
